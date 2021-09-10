552a9deb1f6a3ea25601391c056eb91698efb9c4
Sebastian Glass <s.glass@intershop.de>
2021-09-10 07:40:36 +0200

    better main menu

--- src/app/game/xblrrer/scenes/mainMenuScene/layer/mainmenu.layer.ts
+++ src/app/game/xblrrer/scenes/mainMenuScene/layer/mainmenu.layer.ts
@@ -16,14 +16,23 @@ export default class MainMenuLayer implements RenderLayer {
             filled: true,
         });
         context.drawImage(this.img, 0, 0);
-        const boxX = (FeatherEngine.screenSize.width - 32 * 6) / 2;
-        const boxY = 340;
+        this.drawSelectBox(context, (FeatherEngine.screenSize.width - 32 * 6) / 2, 340);
+        // this.frame.draw(context, 32 * 2, 32 * 2, 384, 64 * 2);
+        this.font.print(
+            'by DialThetti',
+            context,
+            FeatherEngine.screenSize.width - 8 * 13 - 4,
+            FeatherEngine.screenSize.height - 8 - 4,
+        );
+        this.font.print('v0.0.1', context, 4, FeatherEngine.screenSize.height - 8 - 4);
+    }
+
+    drawSelectBox(context: RenderContext, boxX: number, boxY: number): void {
         this.frame.draw(context, boxX, boxY, 32 * 6, 32 * 2);
         this.font.print('Continue', context, boxX + 32, boxY + 16);
         this.font.print('New Game', context, boxX + 32, boxY + 16 + 16 + 8);
         this.font.print('>', context, boxX + 16, boxY + 16 + 24 * this.mainMenu.option);
     }
-
     withZero(count: number, length: number): string {
         return count.toFixed().toString().padStart(length, '0');
     }
