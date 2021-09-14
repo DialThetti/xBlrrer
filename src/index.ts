8017c1bf96ea049ad03b300225cebef7e278fa40
Sebastian Glass <s.glass@intershop.de>
2021-09-14 08:28:11 +0200

    fix: export TiledMapLoader

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
+import TiledMapLoader from './internal/loader/tiled-map.loader';
 import { TiledMap } from './internal/model/tiled-map.model';
 import Tile from './internal/world/tiles/tile';
 import TileMath from './internal/world/tiles/tile.math';
 
-export * from './internal/loader/tiled-map.loader';
 export * from './internal/model';
-export { Tile, TileMath, TiledMap };
+export { Tile, TileMath, TiledMap, TiledMapLoader };
 
