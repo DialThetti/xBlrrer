f253020dd0dc316bd7e34783a17d9aef950efa63
sglass <s.glass@intershop.de>
2021-11-01 07:29:27 +0100

    fix: rename scene event

--- src/app/core/scenes/events.ts
+++ src/app/core/scenes/events.ts
@@ -1,6 +1,6 @@
 import { Subject } from "@dialthetti/feather-engine-events";
 
-export const SHOW_SCENE_EVENT = 'setVolumeSFX';
+export const SHOW_SCENE_EVENT = 'show_scene_event';
 export class ShowSceneEvent implements Subject<{ name: string; withLoading?: boolean; forceLoading?: boolean }> {
     topic = SHOW_SCENE_EVENT;
     constructor(public payload: { name: string; withLoading?: boolean; forceLoading?: boolean }) { }
