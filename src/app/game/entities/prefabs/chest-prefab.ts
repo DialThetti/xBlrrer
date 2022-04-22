d2e0421df348e852072525a2a12c70bb04ed70b2
Sebastian Glaß <s.glass@intershop.de>
2022-04-22 07:42:56 +0200

    feat: chests breakable by attack

--- src/app/game/entities/prefabs/chest-prefab.ts
+++ src/app/game/entities/prefabs/chest-prefab.ts
@@ -3,13 +3,30 @@ import { Entity, EntityPrefab, entityRepo, EntityState } from '@dialthetti/feath
 import PlatformerEntity from '@extension/platformer/entities/platformer-entity';
 
 import { SpawnEvent, TraitAdapter } from 'src/app/core/entities';
-import { Jump, Killable, Player, Stomp } from '../traits';
+import { Killable, Player, Stomp } from '../traits';
 
 
 class ChestTrait extends TraitAdapter {
     prepareForCleanup = false;
+    initialized = false;
     constructor() {
         super('chestTrait');
+    }
+
+    update(entity: PlatformerEntity): void {
+        if (this.initialized)
+            return;
+        const killable = entity.getTrait(Killable);
+        if (killable) {
+
+            if (killable.hp <= 0) {
+                killable.finalize = () => {
+                    this.spawnShiny(entity);
+                    killable.dead = true;
+                };
+                this.initialized = true;
+            }
+        }
     }
     collides(entity: PlatformerEntity, target: Entity): void {
         if (this.prepareForCleanup)
@@ -19,16 +36,7 @@ class ChestTrait extends TraitAdapter {
             return;
         }
         if (target.bounds.bottom > entity.bounds.top && target.bounds.top < entity.bounds.top && target.vel.y > 0) {
-            const value = ChestPrefab.getProperties(entity)['contentValue'];
-
-            for (let index = 0; index < value; index++) {
-                const e = entityRepo['shiny']();
-                e.vel.x = 100 * (Math.random() - Math.random());
-                e.vel.y = -(random(400) + 100);
-                e.state = EntityState.ACTIVE;
-                e.pos.set(entity.pos.x, entity.pos.y);
-                entity.events.publish(new SpawnEvent({ entity: e }));
-            }
+            this.spawnShiny(entity);
 
             this.prepareForCleanup = true;
             if (target.getTrait(Stomp)) {
@@ -45,6 +53,18 @@ class ChestTrait extends TraitAdapter {
 
     }
 
+    spawnShiny(entity: PlatformerEntity): void {
+        const value = ChestPrefab.getProperties(entity)['contentValue'];
+
+        for (let index = 0; index < value; index++) {
+            const e = entityRepo['shiny']();
+            e.vel.x = 100 * (Math.random() - Math.random());
+            e.vel.y = -(random(400) + 100);
+            e.state = EntityState.ACTIVE;
+            e.pos.set(entity.pos.x, entity.pos.y);
+            entity.events.publish(new SpawnEvent({ entity: e }));
+        }
+    }
 
 }
 export class ChestPrefab extends EntityPrefab {
@@ -58,9 +78,9 @@ export class ChestPrefab extends EntityPrefab {
         super('chest', 'chest');
         this.size = new Vector(16, 14);
         this.offset = new Vector(0, 2);
-
         this.traits = (): TraitAdapter[] => [
             new ChestTrait(),
+            new Killable('', 1, 0),
         ];
     }
     entityFac = (): Entity => new PlatformerEntity() as Entity;
