151a0efe9e8652efab31ff1e9fb54aac2bedffc0
Sebastian Glaß <s.glass@intershop.de>
2022-09-08 08:54:51 +0200

    ci: enable tests

--- packages/feather-engine-core/src/internal/io/loaders.spec.ts
+++ packages/feather-engine-core/src/internal/io/loaders.spec.ts
@@ -3,7 +3,7 @@ import { loadImage, loadJson } from './loaders';
 interface Data {
   data: string;
 }
-global.fetch = (name: RequestInfo, init?: RequestInit) =>
+global.fetch = (name: RequestInfo | URL, init?: RequestInit) =>
   new Promise((success, error) => {
     if (name.toString().startsWith('error')) {
       error({ status: 404 } as Response);
@@ -15,22 +15,19 @@ global.fetch = (name: RequestInfo, init?: RequestInit) =>
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 global.Image.prototype.decode = () => {};
 describe('loadJson', () => {
-  it('should load a resource if it exists', async done => {
+  it('should load a resource if it exists', async () => {
     const response = await loadJson('a');
     expect(response).toStrictEqual({ data: 'a' });
-    done();
   });
-  it('should log an error if file not found', async done => {
+  it('should log an error if file not found', async () => {
     const response = await loadJson('error/a');
     expect(response).toStrictEqual({});
-    done();
   });
 });
 
 describe('loadImage', () => {
-  it('should load a resource if it exists', async done => {
+  it('should load a resource if it exists', async () => {
     const response = await loadImage('http://localhost/a');
     expect((response as HTMLImageElement).src).toBe('http://localhost/a');
-    done();
   });
 });
