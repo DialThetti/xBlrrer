9eb71a424fa8ecc5e740d34384628eefed50cd58
Sebastian Glaß <s.glass@intershop.de>
2022-09-09 07:52:26 +0200

    chore: npm update

--- packages/feather-engine-graphics/src/internal/io/sprite-sheet.loader.spec.ts
+++ packages/feather-engine-graphics/src/internal/io/sprite-sheet.loader.spec.ts
@@ -16,27 +16,24 @@ describe('SpriteSheetLoader', () => {
     spriteSpy = spy(spriteSheet);
   });
 
-  it('should load a spritesheet', async done => {
+  it('should load a spritesheet', async () => {
     spriteSheetLoader['loadSheetSpec'] = async () => new Promise(e => e({} as SpriteSheetModel));
     const sprite = await spriteSheetLoader.load();
     expect(sprite).toBe(spriteSheet);
     verify(spriteSpy.define(anything(), anyNumber(), anyNumber(), anyNumber(), anyNumber())).never();
-    done();
   });
-  it('should load a spritesheet with frames', async done => {
+  it('should load a spritesheet with frames', async () => {
     spriteSheetLoader['loadSheetSpec'] = async () =>
       new Promise(e => e({ frames: [{ name: 'a', rect: [0, 0, 0, 0] }] } as SpriteSheetModel));
     const sprite = await spriteSheetLoader.load();
     expect(sprite).toBe(spriteSheet);
     verify(spriteSpy.define('a', 0, 0, 0, 0)).once();
-    done();
   });
-  it('should load a spritesheet with anim', async done => {
+  it('should load a spritesheet with anim', async () => {
     spriteSheetLoader['loadSheetSpec'] = async () =>
       new Promise(e => e({ animations: [{ name: 'a', frames: ['1', '2', '3'], frameLen: 15 }] } as SpriteSheetModel));
     const sprite = await spriteSheetLoader.load();
     expect(sprite).toBe(spriteSheet);
     verify(spriteSpy.defineAnim('a', anyFunction())).once();
-    done();
   });
 });
--- packages/xBlrrer/jest.config.js
+++ packages/xBlrrer/jest.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
-    testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
-    transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
-    moduleNameMapper: {
-        '@engine/(.*)': '<rootDir>/src/app/engine/$1',
-        '@extension/(.*)': '<rootDir>/src/app/extension/$1',
-        '@game/(.*)': '<rootDir>/src/app/game/$1',
-    },
-    modulePaths: [
-        "<rootDir>"
-    ],
-
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '@engine/(.*)': '<rootDir>/src/app/engine/$1',
+    '@extension/(.*)': '<rootDir>/src/app/extension/$1',
+    '@game/(.*)': '<rootDir>/src/app/game/$1',
+  },
+  modulePaths: ['<rootDir>'],
 };
