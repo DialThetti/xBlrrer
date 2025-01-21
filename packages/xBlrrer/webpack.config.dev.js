8d3486e240ae21bb43378b3176e9742911da2b39
Sebastian Paulo <SGlass@intershop.com>
2025-01-21 06:58:41 +0100

    deps: update dependencies

--- packages/xBlrrer/webpack.config.dev.js
+++ packages/xBlrrer/webpack.config.dev.js
@@ -2,21 +2,21 @@ const webpack = require('webpack');
 const webpackConfig = require('./webpack.config');
 
 module.exports = {
-    ...webpackConfig,
+  ...webpackConfig,
 
-    mode: 'development',
-    devtool: 'source-map',
+  mode: 'development',
+  devtool: 'source-map',
 
-    plugins: [
-        ...webpackConfig.plugins,
-        new webpack.LoaderOptionsPlugin({
-            debug: true,
-        }),
-    ],
-    devServer: {
-        contentBase: '../../docs',
-        compress: false,
-        port: 12345,
-    },
-    // Other options...
+  plugins: [
+    ...webpackConfig.plugins,
+    new webpack.LoaderOptionsPlugin({
+      debug: true,
+    }),
+  ],
+  devServer: {
+    static: '../../docs',
+    compress: false,
+    port: 12345,
+  },
+  // Other options...
 };
